ecf6c6939ebcfda5723b1577ddabdf25c52b38f3
Douglas <douglas81silveira@gmail.com>
2023-04-21 13:24:57 -0300

    Arquivos adicionados ao projeto

---
+++ src/main/kotlin/me/dio/democreditapp/DemoCreditApplication.kt
@@ -0,0 +1,11 @@
+package me.dio.democreditapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoCreditApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoCreditApplication>(*args)
+}
---
+++ src/test/kotlin/me/dio/democreditapp/DemoCreditAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.dio.democreditapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoCreditAppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
