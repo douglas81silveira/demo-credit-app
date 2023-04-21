6548360109e016fa5d484ee168606c367825208e
Douglas81 <douglas81silveira@gmail.com>
2023-04-21 15:57:22 -0300

    Implementação da interface CustomerService

--- src/main/kotlin/me/dio/democreditapp/service/ICustomerService.kt
+++ src/main/kotlin/me/dio/democreditapp/service/ICustomerService.kt
@@ -5,5 +5,5 @@ import me.dio.democreditapp.entity.Customer
 interface ICustomerService {
     fun save(customer: Customer): Customer
     fun findById(id: Long): Customer
-    fun delete(id: Long): Customer
+    fun delete(id: Long)
 }
